a22e66e1dc25f0604297acc7c34ce9364d61e060
Diego Borigen <diego@iddigital.com.au>
2025-09-21 18:24:06 +0800

    Update Instragram logic

--- api/getFeed.tsx
+++ api/getFeed.tsx
@@ -1,15 +1,26 @@
 export default async function getFeed() {
-  const token = process.env.INSTAGRAM_TOKEN;
+  const longLivedAccessToken = process.env.INSTAGRAM_TOKEN;
+  const apiUrl = `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${longLivedAccessToken}`;
 
-  const res = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&limit=6&access_token=${token}`);
+   const refreshAccessToken = async () => {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
 
-  const resJson:any = await res.json();
+            const res = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&limit=6&access_token=${data.access_token}`);
+            const resJson:any = await res.json();
+            const result = resJson.data;
 
-  const result = resJson.data;
+            if (!res.ok) {
+                throw new Error('Failed to fetch data')
+            }
 
-  if (!res.ok) {
-      throw new Error('Failed to fetch data')
-  }
+            return result;
 
-  return result;
+        } catch (error) {
+            throw new Error('Failed to refresh token');
+        }
+    };
+
+    return refreshAccessToken();
 }
